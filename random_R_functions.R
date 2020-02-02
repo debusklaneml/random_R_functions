d71e5e384fc4c39c66a85f85d3ec18557869711d
Morgan DeBusk-Lane <morgan.l.debusk@gmail.com>
2020-02-01 23:06:30 -0500

    A working csv function... no arguments though.

--- random_R_functions.R
+++ random_R_functions.R
@@ -1,2 +1,29 @@
 # random R functions for tentative R packages. 
 
+library(tidyverse)
+library(janitor)
+library(readxl)
+library(tidylog)
+
+#Files----
+
+
+tempcsv = dir(pattern=".csv", all.files = T)
+tempxlsx = dir(pattern=".xlsx", all.files = T)
+csv <- map(set_names(tempcsv, tempcsv), read_csv)
+xlsx <- map(set_names(tempxlsx, tempxlsx), read_xlsx)
+
+
+list2env(csv, .GlobalEnv)
+list2env(xlsx, .GlobalEnv)
+
+import <- function(file_type = c("csv", "xlsx", "all")) {
+  
+}
+
+import_all_csv <- function() {
+  tempcsv = dir(pattern=".csv", all.files = T)
+  csv = map(set_names(tempcsv, tempcsv), read_csv)
+  csv <<- csv
+}
+
