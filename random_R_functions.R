2be4b9a027a5ffa8a356e2017d9d6678f56f16d7
Morgan DeBusk-Lane <morgan.l.debusk@gmail.com>
2020-02-11 21:39:20 -0500

    Code Update, need to add print messages next.

--- random_R_functions.R
+++ random_R_functions.R
@@ -17,17 +17,25 @@ xlsx <- map(set_names(tempxlsx, tempxlsx), read_xlsx)
 list2env(csv, .GlobalEnv)
 list2env(xlsx, .GlobalEnv)
 
-import <- function(file_type = c("csv", "xlsx", "all")) {
-  
-}
+# import <- function(file_type = c("csv", "xlsx", "all")) {
+#   
+# }
+# 
+# import_all_csv <- function() {
+#   tempcsv = dir(pattern=".csv", all.files = T)
+#   csv = map(set_names(tempcsv, tempcsv), read_csv)
+#   csv <<- csv
+# }
+# 
+# test_fun <- function(df = FALSE) {
+#   tempcsv = dir(pattern=".csv", all.files = T)
+#   csv = map(set_names(tempcsv, tempcsv), read_csv)
+#   if (df) list2env(csv, .GlobalEnv)
+#   else
+#     csv <<- csv
+# }
 
-import_all_csv <- function() {
-  tempcsv = dir(pattern=".csv", all.files = T)
-  csv = map(set_names(tempcsv, tempcsv), read_csv)
-  csv <<- csv
-}
-
-test_fun <- function(df = FALSE) {
+csv_only <- function(df = FALSE) {
   tempcsv = dir(pattern=".csv", all.files = T)
   csv = map(set_names(tempcsv, tempcsv), read_csv)
   if (df) list2env(csv, .GlobalEnv)
@@ -35,10 +43,21 @@ test_fun <- function(df = FALSE) {
     csv <<- csv
 }
 
-test_fun_1 <- function(df = FALSE) {
-  tempcsv = dir(pattern=".csv", all.files = T)
-  csv = map(set_names(tempcsv, tempcsv), read_csv)
-  if (df) list2env(csv, .GlobalEnv)
+xlsx_only <- function(df = FALSE) {
+  tempxlsx = dir(pattern=".xlsx", all.files = T)
+  xlsx <- map(set_names(tempxlsx, tempxlsx), read_xlsx)
+  if (df) list2env(xlsx, .GlobalEnv)
   else
+    xlsx <<- xlsx
+}
+
+import_all <- function(df = FALSE) {
+  tempcsv = dir(pattern=".csv", all.files = T)
+  tempxlsx = dir(pattern=".xlsx", all.files = T)
+  csv <- map(set_names(tempcsv, tempcsv), read_csv)
+  xlsx <- map(set_names(tempxlsx, tempxlsx), read_xlsx)
+  if (df) list2env(csv, .GlobalEnv) & list2env(xlsx, .GlobalEnv)
+  else
+    xlsx <<- xlsx
     csv <<- csv
 }
