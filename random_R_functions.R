58bdc2c2ebbd499173fb0674f2bb055f708ef624
Morgan DeBusk-Lane <mldebusklane@gmail.com>
2020-03-22 12:17:18 -0400

    Functions

--- random_R_functions.R
+++ random_R_functions.R
@@ -7,33 +7,13 @@ library(tidylog)
 
 #Files----
 
-
 tempcsv = dir(pattern=".csv", all.files = T)
 tempxlsx = dir(pattern=".xlsx", all.files = T)
 csv <- map(set_names(tempcsv, tempcsv), read_csv)
 xlsx <- map(set_names(tempxlsx, tempxlsx), read_xlsx)
 
-
 list2env(csv, .GlobalEnv)
 list2env(xlsx, .GlobalEnv)
-
-# import <- function(file_type = c("csv", "xlsx", "all")) {
-#   
-# }
-# 
-# import_all_csv <- function() {
-#   tempcsv = dir(pattern=".csv", all.files = T)
-#   csv = map(set_names(tempcsv, tempcsv), read_csv)
-#   csv <<- csv
-# }
-# 
-# test_fun <- function(df = FALSE) {
-#   tempcsv = dir(pattern=".csv", all.files = T)
-#   csv = map(set_names(tempcsv, tempcsv), read_csv)
-#   if (df) list2env(csv, .GlobalEnv)
-#   else
-#     csv <<- csv
-# }
 
 csv_only <- function(df = FALSE) {
   tempcsv = dir(pattern=".csv", all.files = T)
